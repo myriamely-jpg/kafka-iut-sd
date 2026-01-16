7992a124d47c8eeaebe2648c61fb20c51d77fbd6
myriam <myriam.behren@gmail.com>
2026-01-16 12:59:57 +0000

    modif 1

--- python-producer/app.py
+++ python-producer/app.py
@@ -2,21 +2,25 @@ from kafka import KafkaProducer
 import json
 
 # Define the Kafka broker and topic
-broker = '{RPI_IP}:9092'
+broker = 'my-kafka.elyaago18u-dev.svc.cluster.local:9092'
 topic = 'my-first-topic'
 
 # Create a Kafka producer
-#producer = KafkaProducer(
-#    bootstrap_servers=[broker],
-#   sasl_mechanism='SCRAM-SHA-256',
-#     security_protocol='SASL_PLAINTEXT',
-#     sasl_plain_username='user1',
-#     sasl_plain_password='FHp2sfyGox'
-#)
 producer = KafkaProducer(
     bootstrap_servers=[broker],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+   sasl_mechanism='SCRAM-SHA-256',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+     security_protocol='SASL_PLAINTEXT',
+     sasl_plain_username='user1',
+     sasl_plain_password='t2pJjbfnz3'
 )
+#producer = KafkaProducer(
+ #   bootstrap_servers=[broker],
+ #   value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+  #  sasl_plain_password = "t2pJjbfnz3",
+ #   sasl_plain_username ="user1"
+
+#)
 # Define the message to send
 message = {
     'key': 'this-is-a-key',
